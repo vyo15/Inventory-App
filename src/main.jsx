a3d01228b97c7f7179ff192c152bbb1638c801b6
Vio Yusuf Iskandar <vio@pabrik.cslgroup.lan>
2025-09-10 13:41:02 +0700

    Test HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
