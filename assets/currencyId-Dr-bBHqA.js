ec11db0d98167a470780ed1f73c95889528a5c72
Vio Yusuf <you@example.com>
2026-04-25 00:21:41 +0700

    Updates

--- assets/currencyId-Dr-bBHqA.js
+++
@@ -1,1 +0,0 @@
-const e=(t,m=!0)=>{const r=new Intl.NumberFormat("id-ID",{maximumFractionDigits:0}).format(Number(t||0));return m?`Rp ${r}`:r};export{e as f};
--- assets/numberId-D1L6gPKy.js
+++
@@ -1,1 +0,0 @@
-const a=r=>{const t=Number(r||0);return new Intl.NumberFormat("id-ID",{maximumFractionDigits:0}).format(t)},m=a;export{m as a,a as f};
